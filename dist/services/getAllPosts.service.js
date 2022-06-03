e7d4953016870674d3db8a0d87c0ce9ad7d784c7
g-reissantana <76252582+g-reissantana@users.noreply.github.com>
2022-06-02 23:18:49 -0300

    Delete dist directory

--- dist/services/getAllPosts.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllPosts = void 0;
-const prisma_1 = require("./prisma");
-const getAllPosts = async () => {
-    const allPosts = await prisma_1.prisma.imgs.findMany();
-    return allPosts;
-};
-exports.getAllPosts = getAllPosts;
