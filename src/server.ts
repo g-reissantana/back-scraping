74e47d74a9a8a55d59d3ff2c77c4685fe74b5830
Gabriel Reis <gabriel.familia32@hotmail.com>
2022-06-02 23:12:57 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use(cors);
+app.use(cors());
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('Server is running!')
